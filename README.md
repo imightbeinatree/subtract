0bbf00b941bde6d132aaee24119bffb428dcc388
Michael Orr <michael@cloudspace.com>
2015-03-09 11:46:14 -0400

    initial commit without binary

---
+++ README.md
@@ -0,0 +1,3 @@
+# Go Subtract Integers!
+
+A go program which takes in two arguments that should both be integers and outputs a json object containing one key "result".
---
+++ integer_subtract.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "strconv"
+)
+
+func main() {
+  a, _ := strconv.Atoi(os.Args[1])
+  b, _ := strconv.Atoi(os.Args[2])
+  result := strconv.Itoa(a - b)
+  fmt.Println("{\"result\":\""+result+"\"}")
+}
+
+
